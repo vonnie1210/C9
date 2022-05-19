3e03afddbda738e9c5b46a33914697b0c0f3b340
vonnie1210 <105844890+vonnie1210@users.noreply.github.com>
2022-05-19 17:25:01 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var ball;
+
+function setup() {
+  createCanvas(400,400);
+
+  ball = createSprite(200, 200, 30, 30);
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyIsDown(RIGHT_ARROW)){
+    ball.position.x = ball.position.x + 5;
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    ball.position.x = ball.position.x - 5;
+  }
+
+  if(keyIsDown(DOWN_ARROW)){
+    ball.position.y = ball.position.y + 5;
+  }
+
+  if(keyIsDown(UP_ARROW)){
+    ball.position.y = ball.position.y - 5;
+  }
+
+  drawSprites();
+}
+
+
+
+
